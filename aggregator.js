1f4a1f4240d5a1c08211b183e041dfe35018e023
Dmitriy Kurkin <dm-kurkin@yandex-team.ru>
2018-09-25 13:23:25 +0300

    Multichannel aggregator

--- aggregator.js
+++ aggregator.js
@@ -7,23 +7,42 @@ const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 const bot = new TelegramBot(config.token);
+class TGChannel {
+    constructor(channel, feeds) {
+        this.index = 0;
+        this.reciever = channel
+        this.channels = []
+        feeds.forEach( (url) => { 
+            this.channels.push( new RSSChannel(url) ); 
+        });    
+        if( this.channels.length > 0 ) {
+            this.checkNews();
+        }
+    }
+
+    processItems(items) {
+        items.forEach((item) => { 
+            bot.sendMessage(this.reciever, `[${item.title}](${item.link})`,{parse_mode : "Markdown"});
+        });
+        this.index+=1;
+        if(this.index>=this.channels.length) {
+            this.index = 0;
+        }
+        const that = this
+        setTimeout(() => {
+            that.checkNews();
+        }, config.timeout);
+    }
+
+    checkNews() {
+        const that = this
+        this.channels[this.index].getNews((items)=>{ that.processItems(items) });
+    }
+
+}  
+
 const channels = [];
-config.feeds.forEach( (url) => { 
-    channels.push( new RSSChannel(url) ); 
+config.tgchannels.forEach( (ch) => { 
+    channels.push( new TGChannel(ch.channel, ch.feeds) ); 
 });
 
-let index = 0;
-channels.length && channels[index].getNews(processItems);
-
-function processItems(items) {
-    items.forEach((item) => { 
-        bot.sendMessage(config.channel, `[${item.title}](${item.link})`,{parse_mode : "Markdown"});
-    });
-    index+=1;
-    if(index>=channels.length) {
-        index = 0;
-    }
-    setTimeout(() => {
-        channels[index].getNews(processItems);
-    }, config.timeout);
-};
